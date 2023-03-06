194c90e9ca972f6ab7f433ab6c01c82707931ccc
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-06 15:23:16 +0530

    TestCase for mail

---
+++ UnitTest.cs
@@ -0,0 +1,28 @@
+﻿
+using UserRegistrationProblem;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string firstName = "shruthi";
+            string lastName = "chirra";
+            string email = "siri@gmail.com";
+            string mobile = "624752";
+            string password = "sunny678";
+            string expectedOutput = "SAD";
+            EmailValidate e1 = new EmailValidate();
+
+            //Act
+            string actualOutput = e1.validate(firstName, lastName, email, mobile, password);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+
+        }
+    }
+}
