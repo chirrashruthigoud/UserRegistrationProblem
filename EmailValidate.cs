d950ee7c5fd51aff5d488c1fe5c3f2bb54f0385b
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-06 15:23:00 +0530

    TestCase for mail

---
+++ EmailValidate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+     class EmailValidate
+    {
+        public string validate(string firstName, string lastName, String email, string mobile, string password)
+        {
+            if (firstName == "" && lastName == "" && email == "" && mobile == "" && password == "")
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
