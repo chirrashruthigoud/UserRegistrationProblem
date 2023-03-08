af4aeb504a89105b4e0451806f8e58158fbb99c9
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-08 18:21:24 +0530

    Test to validate multiple entry for the Email Address

--- Program.cs
+++ Program.cs
@@ -4,10 +4,34 @@ namespace UserRegistrationProblem
 {
     class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an email address : ");
+            string email = Console.ReadLine();
+
+
+            if (ValidateEmail(email))
+            {
+                Console.WriteLine("The email address is valid.");
+            }
+            else
+            {
+                Console.WriteLine("The email address is invalid.");
+            }
+        }
+        static bool ValidateEmail(string email)
         {
 
+            string emailRegex = @"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$";
 
+
+            Match match = Regex.Match(email, emailRegex);
+
+
+            return match.Success;
         }
+
+
     }
+    
 }
