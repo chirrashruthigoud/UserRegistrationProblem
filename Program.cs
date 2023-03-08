b52eb834259371e45cbfbdb872123e4e6d0ffa2a
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-08 18:35:04 +0530

    use Lambda Function to validate User Entry

--- Program.cs
+++ Program.cs
@@ -2,21 +2,27 @@
 
 namespace UserRegistrationProblem
 {
-    class Program
+   public class Program
     {
         public static void Main(string[] args)
-        {
-            try
-            {
-                EmailValidate e1 = new EmailValidate();
-                e1.validate("sagar", "shahu", "shahu@gmail.com", "555666", "pass123");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+        { // Define lambda functions for validation
+            Func<string, bool> validateFirstName = name => Regex.IsMatch(name, @"^[A-Za-z]{2,20}$");
+            Func<string, bool> validateLastName = name => Regex.IsMatch(name, @"^[A-Za-z]{2,20}$");
+            Func<string, bool> validateEmail = email => Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+            Func<string, bool> validateMobile = mobile => Regex.IsMatch(mobile, @"^[0-9]{10}$");
+            Func<string, bool> validatePassword = password => Regex.IsMatch(password, @"^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[^\da-zA-Z]).{8,20}$");
 
-
+            // Test the lambda functions
+            Console.WriteLine(validateFirstName("John")); // true
+            Console.WriteLine(validateFirstName("J")); // false
+            Console.WriteLine(validateLastName("Doe")); // true
+            Console.WriteLine(validateLastName("D")); // false
+            Console.WriteLine(validateEmail("john.doe@example.com")); // true
+            Console.WriteLine(validateEmail("john.doe@.com")); // false
+            Console.WriteLine(validateMobile("1234567890")); // true
+            Console.WriteLine(validateMobile("123456")); // false
+            Console.WriteLine(validatePassword("Passw0rd!")); // true
+            Console.WriteLine(validatePassword("password")); // false
         }
     }
 }
